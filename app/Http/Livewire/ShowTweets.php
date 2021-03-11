3a1f2d0ff0759fc4ca0bfe7c39bdb6d8aca86286
Anderson Alves <anderson.mand.as@hotmail.com>
2021-03-11 16:17:07 -0300

    Inserindo registros com livewire

--- app/Http/Livewire/ShowTweets.php
+++ app/Http/Livewire/ShowTweets.php
@@ -12,7 +12,18 @@ class ShowTweets extends Component
     public function render()
     {
         // Recuperar todos os tweets
-        $tweets = Tweet::get();
+        $tweets = Tweet::with('user')->get();
+
         return view('livewire.show-tweets', compact('tweets'));
+    }
+
+    public function create()
+    {
+        Tweet::create([
+            'content' => $this->message,
+            'user_id' => 1,
+        ]);
+
+        $this->message = '';
     }
 }
--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -3,8 +3,12 @@
     <br>
     {{ $message }}
     <br>
-    <input type="text" name="message" id="message" wire:model="message">
-
+    <hr>
+    <form action="" method="post" wire:submit.prevent="create">
+        <input type="text" name="message" id="message" wire:model="message">
+        <button type="submit">Salvar</button>
+    </form>
+    <hr>
     <h1>Lista de todos os Tweets</h1>
     @foreach ($tweets as $tweet)
         {{ $tweet->user->name }} - {{ $tweet->content }}. <br>
